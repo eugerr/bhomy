ef63fe75fe08c522f1915dc6d73ecdf9adb64662
eugerr <gardensensei4@gmail.com>
2024-05-29 08:31:47 -0700

    added nativewind and dark mode

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,15 @@
+import { Text, View } from '@/components/Themed';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
 import React from 'react';
-import { Switch, Text, View } from 'react-native';
+import { Switch } from 'react-native';
 
 export default function App() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
-    <View className='flex-1 items-center justify-center dark:bg-black'>
+    <View className='flex-1 items-center justify-center h-screen'>
       <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Text className='dark:text-white'>Toggle theme</Text>
+      <Text>Toggle theme</Text>
       <Switch value={colorScheme === 'dark'} onChange={toggleColorScheme} />
       <StatusBar style='auto' />
     </View>
--- src/components/Themed.tsx
+++ src/components/Themed.tsx
@@ -6,7 +6,7 @@
 import { Text as DefaultText, View as DefaultView } from 'react-native';
 
 import Colors from '@/constants/Colors';
-import { useColorScheme } from './useColorScheme';
+import { useColorScheme } from 'nativewind';
 
 type ThemeProps = {
   lightColor?: string;
@@ -20,7 +20,8 @@ export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
 ) {
-  const theme = useColorScheme() ?? 'light';
+  const { colorScheme } = useColorScheme();
+  const theme = colorScheme ?? 'light';
   const colorFromProps = props[theme];
 
   if (colorFromProps) {
@@ -34,12 +35,27 @@ export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
-  return <DefaultText style={[{ color }, style]} {...otherProps} />;
+  return (
+    <DefaultText
+      className='dark:text-white'
+      style={[{ color }, style]}
+      {...otherProps}
+    />
+  );
 }
 
 export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    'background'
+  );
 
-  return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+  return (
+    <DefaultView
+      className='dark:bg-black'
+      style={[{ backgroundColor }, style]}
+      {...otherProps}
+    />
+  );
 }
